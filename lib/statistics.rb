2e0b21772753170a3278b314f13c5f2ad30e3e29
JohnnyGsy <vanstrelok@gmail.com>
2021-09-14 07:42:12 +0300

    game is done without rspec

---
+++ lib/statistics.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Statistics
+  include Storage
+
+  attr_reader :games, :file
+
+  def initialize(path)
+    @file = path
+    @games = load_games
+  end
+
+  def add_game(game)
+    @games << game.game_data
+  end
+
+  def save_games(path = @file)
+    @games.sort_by! { |game| game[:rating] }
+    data = { games: @games }
+    save(data, path)
+  end
+
+  def load_games(path = @file)
+    data = load(path) if File.exist?(path)
+    return [] unless data
+
+    data[:games]
+  end
+end

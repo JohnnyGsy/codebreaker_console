050ae7ca10686cafff744ee68bda2058da1c6920
JohnnyGsy <vanstrelok@gmail.com>
2021-09-15 22:34:05 +0300

    added module codebreaker

--- icodebreaker_gem.gemspec
+++ icodebreaker_gem.gemspec
@@ -24,12 +24,14 @@ Gem::Specification.new do |spec|
   spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
-
+ 
+  spec.add_dependency 'rake', '~> 13.0'
+ 
   spec.add_development_dependency 'fasterer'
   spec.add_development_dependency 'pry'
   spec.add_development_dependency 'rspec', '~> 3.0'
-  spec.add_development_dependency 'rubocop', '~> 1.7'
   spec.add_development_dependency 'simplecov'
+  spec.add_development_dependency 'rubocop', '~> 1.7'
 
-  spec.add_dependency 'rake', '~> 13.0'
+  
 end
--- lib/code.rb
+++ lib/code.rb
@@ -1,50 +1,50 @@
-# frozen_string_literal: true
+module IcodebreakerGem 
+  class Code
+    include Validation
 
-class Code
-  include Validation
+    attr_reader :secret
 
-  attr_reader :secret
+    def initialize(secret = Code.random)
+      validate_code secret
 
-  def initialize(secret = Code.random)
-    validate_code secret
-
-    @secret = secret.to_s
-    @shuffle = @secret.each_char.to_a.shuffle
-    @hint_idx = 0
-  end
-
-  def compare(guess)
-    return '++++' if @secret == guess
-    return '' if (@secret.each_char.to_a & guess.each_char.to_a).empty?
-
-    bulls1, cows1 = bulls_and_cows(@secret, guess)
-    bulls2, cows2 = bulls_and_cows(guess, @secret)
-
-    '+' * [bulls1, bulls2].min + '-' * [cows1, cows2].min
-  end
-
-  def hint
-    result = @shuffle[@hint_idx]
-    @hint_idx += 1
-    @hint_idx = 0 if @hint_idx >= @shuffle.size
-    result
-  end
-
-  def self.random
-    (0..3).map { rand(1..6) }.map(&:to_s).join
-  end
-
-  private
-
-  def bulls_and_cows(word1, word2)
-    bulls = cows = 0
-    word1.each_char.to_a.each_index do |index|
-      if word2[index] == word1[index]
-        bulls += 1
-      elsif word2.include? word1[index]
-        cows += 1
-      end
+      @secret = secret.to_s
+      @shuffle = @secret.each_char.to_a.shuffle
+      @hint_idx = 0
     end
-    [bulls, cows]
+
+    def compare(guess)
+      return '++++' if @secret == guess
+      return '' if (@secret.each_char.to_a & guess.each_char.to_a).empty?
+
+      bulls1, cows1 = bulls_and_cows(@secret, guess)
+      bulls2, cows2 = bulls_and_cows(guess, @secret)
+
+      '+' * [bulls1, bulls2].min + '-' * [cows1, cows2].min
+    end
+
+    def hint
+      result = @shuffle[@hint_idx]
+      @hint_idx += 1
+      @hint_idx = 0 if @hint_idx >= @shuffle.size
+      result
+    end
+
+    def self.random
+      (0..3).map { rand(1..6) }.map(&:to_s).join
+    end
+
+    private
+
+    def bulls_and_cows(word1, word2)
+      bulls = cows = 0
+      word1.each_char.to_a.each_index do |index|
+        if word2[index] == word1[index]
+          bulls += 1
+        elsif word2.include? word1[index]
+          cows += 1
+        end
+      end
+      [bulls, cows]
+    end
   end
 end

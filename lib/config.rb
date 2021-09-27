6d4d68eb7cb877ca50bda436196b4f5d80931286
JohnnyGsy <vanstrelok@gmail.com>
2021-09-27 05:19:29 +0300

    test files deleted

--- lib/config.rb
+++ lib/config.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+
+require 'yaml'
+
 require 'modules/validation'
 require 'modules/storage'
-require 'pry'
+
 require 'game_core'
 require 'game'
-require 'yaml'
 require 'icodebreaker_gem/version'
 require 'icodebreaker_gem'
 require 'constants'
